c41d4d344cdb9fd68e781c35a9bb6430791acfba
Jurgita Butiene <jurgita02@yahoo.com>
2023-05-03 17:11:55 +0200

    added long forgotten code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите слово");
+        String word = scanner.next();
+        System.out.println("Слово: \"" + word + "\"");
+        int numberOfCharacters = word.length();
+        System.out.println("Количество букв: " + numberOfCharacters);
+        char firstSymbol = word.charAt(0);
+        char lastSymbol = word.charAt(word.length() - 1);
+        System.out.println("Начинается на: " + firstSymbol);
+        System.out.println("Заканчивается на: "+ lastSymbol);
+
+
     }
 }
